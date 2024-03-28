1224e2817c2f624ef7c690ddf4aef97419cb05b5
Hamza Ahmed <gboy76304@gmail.com>
2024-03-28 18:26:12 +0500

    25 && 26 && 27

---
+++ day27.ts
@@ -0,0 +1,49 @@
+/*
+Question 79: Creating and Accessing Object Properties: Construct an object representing a car with properties for make, model, and year. Then, show how you can access the model property of the car.
+*/
+
+
+let specialCar = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+
+console.log(specialCar.model);
+
+/*
+Question 80: Updating Object Properties: Add a property named color to the existing car object, and then update the year property to 2021. Show how to perform these operations.
+*/
+
+
+// Starting with our car object
+ specialCar = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+
+car.color = "blue";
+car.year = 2021;
+
+// Showing the updated car object
+console.log(car);
+
+
+
+/*
+Question 81: Iterating Over Object Properties: Write a function that takes an object as an argument and logs all of its properties and values.
+*/
+
+
+// This function shows every detail about an object
+function logObjectProperties(obj: any) {
+    for (let property in obj) {
+        
+        console.log(`${property}: ${obj[property]}`);
+      
+    }
+}
+
+// Using the function with a car object
+logObjectProperties({ make: "Toyota", model: "Corolla", year: 2021, color: "blue" });
