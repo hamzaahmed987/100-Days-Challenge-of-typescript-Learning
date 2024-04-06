81e9ea3ebebc9e64aeb49826486377665e97fb95
Hamza Ahmed <gboy76304@gmail.com>
2024-04-06 22:07:14 +0500

    code

---
+++ day34.ts
@@ -0,0 +1,25 @@
+/*
+Question 100: Use the JavaScript Math object to find the square root of 144.
+*/
+const squareRoot: number = Math.sqrt(144);
+
+console.log(squareRoot); 
+
+
+/*
+Question 101: Generate a random integer between 1 and 10.
+*/
+function getRandomInt(): number {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+console.log(getRandomInt()); 
+
+
+
+/*
+Question 102: Calculate and log the absolute difference between the number -5 and 5.
+*/
+const difference: number = Math.abs(-5 - 5);
+
+console.log(difference);
