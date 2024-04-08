21a7303743421d353aebad151d48ddfd5f5e53c9
Hamza Ahmed <gboy76304@gmail.com>
2024-04-08 20:39:39 +0500

    last commit

---
+++ day37.js
@@ -0,0 +1,44 @@
+// /*
+// Question 109: Write an if statement that logs "Good Morning" if the current time is before 12 PM.
+// */
+// //     checl hour and logs good ,morning if it's before 12 PM
+// const currentTime = new Date();
+// if (currentTime.getHours() < 12) {
+//   console.log("Good Morning");
+// }
+// /*
+// Question 110: Create a function that assigns a grade (A, B, C, D, F) based on a student's score.
+// */
+// function assignGrade(score: number): string {
+//     if (score >= 90) {
+//       return "A";
+//     } else if (score >= 80) {
+//       return "B";
+//     } else if (score >= 70) {
+//       return "C";
+//     } else if (score >= 60) {
+//       return "D";
+//     } else {
+//       return "F";
+//     }
+//   }
+//   console.log(assignGrade(45));
+//   console.log(assignGrade(95)); 
+/*
+Question 111: Use an if-else-if chain to categorize a person's age group (child, teenager, adult).
+*/
+// This function categorizes a person's age group
+function categorizeAge(age) {
+    if (age < 13) {
+        return "child";
+    }
+    else if (age <= 19) {
+        return "teenager";
+    }
+    else {
+        return "adult";
+    }
+}
+console.log(categorizeAge(12));
+console.log(categorizeAge(18));
+console.log(categorizeAge(25));
---
+++ day38.ts
@@ -0,0 +1,47 @@
+// /*
+// Question 112: Create a Map that stores the names of countries as keys and their capitals as values. Add three countries to the Map.
+// */
+// const countries = new Map<string, string>();
+// countries.set("Pakistan", "Washington, D.C.");
+// countries.set("USA", "Washington, D.C.");
+// countries.set("France", "Paris"); 
+// countries.set("Japan", "Tokyo"); 
+
+// console.log(countries);
+
+
+
+// /*
+// Question 113: Write a function that checks if a Map contains a key for "Canada" and logs the capital if it exists.
+// */
+
+// function capitalOfCanada(countries: Map<string, string>): void {
+//   if (countries.has("Canada")) {
+//     console.log(`The capital of Canada is ${countries.get("Canada")}`);
+//   } else {
+//     console.log("Canada is not in the Map.");
+//   }
+// }
+
+// capitalOfCanada(countries);
+
+
+
+
+
+
+
+/*
+Question 114: Iterate over a Map of student IDs and names, and log each pair to the console.
+*/
+
+
+// Creates a Map to store student IDs (keys) and names (values)
+const students = new Map<number, string>();
+students.set(1, "Hamza");
+students.set(2, "Ahmed");
+students.set(3, "Shayan");
+
+students.forEach((name, id) => {
+  console.log(`Student ID: ${id}, Name: ${name}`);
+});
