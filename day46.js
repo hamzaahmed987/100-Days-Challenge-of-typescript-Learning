070a6a4e137f334b35b66bea59ea958161e32334
Hamza <gboy76304@gmail.com>
2024-06-25 05:50:49 -0700

    updated

---
+++ day46.js
@@ -0,0 +1,27 @@
+"use strict";
+// /*
+// Question 136: Use console.log() to debug and track the value of a variable inside a loop.
+// */
+// for (let i = 1; i <= 5; i++) {
+//     console.log(`Iteration ${i}, i value:, i`); 
+//   }
+Object.defineProperty(exports, "__esModule", { value: true });
+// /*
+// Question 137: Implement a try-catch block to handle potential errors in a block of code.
+// */
+// let a = 6;
+// try {
+//     if (a == 5) {
+//         console.log(`a is`,a);
+//     } else {
+//         throw new Error("a is not equal to 5");
+//     }
+// } catch (error) {
+//     console.log(error.message);
+// }
+/*
+Question 138: Describe how to use breakpoints in browser developer tools to debug JavaScript code.
+*/
+console.log("Before breakpoint");
+console.log("This line has a breakpoint set on it in the browser's developer tools");
+console.log("After breakpoint");
