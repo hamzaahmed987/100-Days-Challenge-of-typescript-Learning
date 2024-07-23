b2a32ae022b351116b489b8c8233e761af2c3bd0
Hamza <gboy76304@gmail.com>
2024-07-23 09:15:18 -0700

    update

---
+++ day73/TS-Type-Assertions/01-Type-Assertions.js
@@ -0,0 +1,5 @@
+//==========> TYPE ASSERTIONS <============\\
+let data = "999999";
+// TODO: Type assertion to treat "data" as a string
+console.log(data.repeat(3));
+export {};
---
+++ day73/TS-Type-Assertions/01-Type-Assertions.ts
@@ -0,0 +1,9 @@
+
+//==========> TYPE ASSERTIONS <============\\
+
+
+
+let data: any = "999999";
+
+// TODO: Type assertion to treat "data" as a string
+console.log((data as string).repeat(3));
